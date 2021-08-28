ba8943f3214d9afb883146b092bea82ce93454f9
koooota <eno6k21@gmail.com>
2021-08-28 16:28:59 +0900

    Create a new project

---
+++ README.md
@@ -0,0 +1,6 @@
+# hts_buddygram
+
+## Requirement
+
+- Dart 2.13.x
+- Flutter 2.2.3 (channel stable)
---
+++ android/app/src/main/kotlin/com/example/hts_buddygram/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.hts_buddygram
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
